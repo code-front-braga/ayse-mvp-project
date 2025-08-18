f05d7ed475fe96ec5ebacbf82fcdc6adcf627941
Leonardo Braga <programer_frontbraga@outlook.com>
2025-08-17 21:23:34 -0300

    feat: implement metadata to improve SEO

--- src/app/(apresentation)/page.tsx
+++ src/app/(apresentation)/page.tsx
@@ -1,9 +1,47 @@
+import { Metadata } from 'next';
+
 import LandingBenefitsSection from '../components/landing-benefits-section';
 import LandingCtaSection from '../components/landing-cta-section';
 import LandingFeaturesSection from '../components/landing-features-section';
 import LandingFooter from '../components/landing-footer';
 import LandingHeader from '../components/landing-header';
 import LandingHeroSection from '../components/landing-hero-section';
+
+export const metadata: Metadata = {
+	title: 'ayse - all your supermarket expenses',
+	description: 'Acesse sua conta e comece a usar o Ayse',
+	keywords: [
+		'supermercados',
+		'compras',
+		'financeiro',
+		'ayse',
+		'dinheiro',
+		'nextjs',
+	],
+	authors: [{ name: 'Leonardo Braga' }],
+	openGraph: {
+		images: [`${process.env.PROJECT_URL}/landing.png`],
+		url: `${process.env.PROJECT_URL}`,
+		siteName: 'Ayse',
+		title: 'ayse - all your supermarket expenses',
+		description:
+			'Aplicação MVP desenvolvida com Next.js e tecnologias modernas',
+		type: 'website',
+		locale: 'pt_BR',
+	},
+	robots: {
+		index: true,
+		follow: true,
+		nocache: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			noimageindex: true,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+		},
+	},
+};
 
 const LandingPage = () => {
 	return (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,8 @@
 import '@/app/styles/globals.css';
 
-import type { Metadata } from 'next';
-
 import { Toaster } from '@/components/ui/sonner';
 
 import { fonts } from './styles/fonts';
-
-export const metadata: Metadata = {
-	title: 'ayse',
-	description: 'Aplicação MVP desenvolvida com Next.js e tecnologias modernas',
-	keywords: [
-		'Next.js',
-		'React',
-		'TypeScript',
-		'Tailwind CSS',
-		'MVP',
-		'Supermarket',
-		'Finance',
-		'Control',
-		'Money',
-		'Dashboard',
-		'Panel',
-	],
-	authors: [{ name: 'Leonardo Braga' }],
-	openGraph: {
-		title: 'ayse - all your supermarket expenses',
-		description:
-			'Aplicação MVP desenvolvida com Next.js e tecnologias modernas',
-		type: 'website',
-		locale: 'pt_BR',
-	},
-};
 
 interface RootLayoutProps {
 	children: React.ReactNode;
